fe5d1674f3dbe2b54262dcfc221ca0daabb59923
TobyW92 <tobywright@live.com>
2024-08-19 17:15:46 -0400

    completed lesson 32-coffee-smoke

---
+++ 32-coffee-smoke-shader/src/shaders/coffeeSmoke/fragment.glsl
@@ -0,0 +1,33 @@
+uniform float uTime;
+uniform sampler2D uPerlinTexture;
+
+varying vec2 vUv;
+
+void main() {
+
+    vec2 smokeUv = vUv;
+
+    // Adjust Texture Size
+    smokeUv.x *= 0.5;
+    smokeUv.y *= 0.3;
+
+    // Animate across Y Axis
+    smokeUv.y -= uTime * 0.05;
+
+    float smoke = texture(uPerlinTexture, smokeUv).r;
+    smoke = smoothstep(0.4, 1.0, smoke);
+
+    // Edges
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
+
+
+    gl_FragColor = vec4(0.6, 0.3, 0.2, smoke);
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
